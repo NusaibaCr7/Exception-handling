f2c90d0a73aee174eea3f8bb2cd864335551aaf0
Nusaiba Faiza <nusaiba.faiza@gmail.com>
2024-09-11 18:36:36 +0600

    first commit

---
+++ value error.py
@@ -0,0 +1,9 @@
+try:
+    x = int(input("Enter a number: "))
+    print(x)
+    
+except ValueError as e:
+    print(e)
+    
+z = 15 + 35
+print(f"Backup ready mama with {z} genjam gang manushjon🗣🔥🔥")
